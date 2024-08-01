3b1434d4ddf9502b750065a4163c7366086c5613
Len Bekker <len.bekker@sybrin.com>
2024-08-01 14:10:49 +0200

    docs: annotate functions and methods

--- src/services/authenticationFactory.ts
+++ src/services/authenticationFactory.ts
@@ -8,6 +8,14 @@ export class AuthenticationService implements IAuthenticationService {
     this.authService = new KeycloakService();
   }
 
+  /**
+   * Authenticates with the provided username and password via given authService
+   * Generates a TazamaToken string embedded with claims pulled from the custom service
+   *
+   * @param {string} username - The username for authentication.
+   * @param {string} password - The password for authentication.
+   * @returns {Promise<string>} - A promise that resolves to a signed JWT token.
+   */
   async getToken(username: string, password: string): Promise<string> {
     return await this.authService.getToken(username, password);
   }
