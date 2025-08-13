b23a5dffe89f1e6d755cd7e85ea65e0d196c8d1a
Abdullah-Siddiqui-Paysys <abdullah.siddiqui@paysyslabs.com>
2025-08-13 11:56:41 +0500

    feat: Add tenant validation service from TMS Service to Auth-Lib Service

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import * as AuthProviderConfig from './services/providerHelper';
 import * as JwtService from './services/jwtService';
 import { TazamaAuthentication, type TazamaAuthProvider } from './services/tazamaAuthentication';
 import { validateTokenAndClaims } from './services/tazamaService';
+import { validateAndExtractTenant, validateTokenAndExtractTenant, type TenantValidationResult, type TenantValidationOptions } from './services/tenantService';
 
 // Providers
 export { AuthProviderConfig, JwtService, TazamaAuthentication };
 export type { TazamaAuthProvider, TazamaToken };
 
 // Clients
-export { validateTokenAndClaims };
-export type { ClaimValidationResult };
+export { validateTokenAndClaims, validateAndExtractTenant, validateTokenAndExtractTenant };
+export type { ClaimValidationResult, TenantValidationResult, TenantValidationOptions };
